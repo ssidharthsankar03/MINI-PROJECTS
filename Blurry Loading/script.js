351fab9d9faa928de3c16484264bb72b3000330b
ssidharthsankar03 <127672323+ssidharthsankar03@users.noreply.github.com>
2024-08-13 22:10:34 +0530

    Add files via upload

---
+++ Blurry Loading/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loadtext = document.querySelector('.loadingtext');
+    const bg = document.querySelector('.bg');
+
+    let load = 0;
+    let int = setInterval(blurring, 30);
+
+    function blurring() {
+        load++;
+        if (load > 99) {
+            clearInterval(int);
+        }
+        loadtext.innerText = `${load}%`;
+        loadtext.style.opacity = scale(load, 0, 100, 1, 0);
+        bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+    }
+
+    const scale = (number, inMin, inMax, outMin, outMax) => {
+        return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    };
+});
